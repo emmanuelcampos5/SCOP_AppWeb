d11ecd7d587053314584a0d47c1e6ddcc6a411d7
indieferente97 <m.johel1397@gmail.com>
2023-11-20 15:37:20 -0600

    Se realiza la iauditoria de eliminación de órdenes de producción

--- SCOP_AppWeb/obj/Debug/net6.0/SCOP_AppWeb.AssemblyInfo.cs
+++ SCOP_AppWeb/obj/Debug/net6.0/SCOP_AppWeb.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfb419801afd1895abc49c32a094682c1ed9bb0")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
