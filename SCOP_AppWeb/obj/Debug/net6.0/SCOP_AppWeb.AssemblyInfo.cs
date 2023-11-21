7c57ea8e8662b08147e70048da121e06258002c7
emmanuelcampos5 <94939174+emmanuelcampos5@users.noreply.github.com>
2023-11-21 07:46:06 -0600

    registrar Devoluciones

--- SCOP_AppWeb/obj/Debug/net6.0/SCOP_AppWeb.AssemblyInfo.cs
+++ SCOP_AppWeb/obj/Debug/net6.0/SCOP_AppWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bb7ac2aa888b5a9aa5d988c29f649100664798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be3f239bf2533e00432d909982f81a62e07058b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SCOP_AppWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
